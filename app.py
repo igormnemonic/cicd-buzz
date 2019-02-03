9508d73c4b53c726b72ec6d9334f0930cf39badb
igormnemonic <igor.kamenetsky@gmail.com>
2019-02-03 22:31:43 +0100

    Adjusting formating

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
+    page = '<html><head><title>CS Planning Agile Transformation moto</title></head><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
